57ffab9c92f33159078286f3d667438db990e817
stephentian <745752067@qq.com>
2023-04-24 15:54:17 +0800

    :sparkles: feat:[js] deepClone

--- js/code/deepClone.js
+++ js/code/deepClone.js
@@ -11,3 +11,14 @@ function clone(target) {
     return target
   }
 }
+
+// 深拷贝优化
+// 正则、时间类型处理
+// 函数处理
+// 循环引用问题
+
+function deepClone(obj, hash= new WeakMap()) {
+  if (obj === null) return obj
+  if (obj instanceof RegExp) return new RegExp(obj)
+  if (obj instanceof Date) return new Date(obj)
+}
