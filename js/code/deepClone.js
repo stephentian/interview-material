573470770ded7ccb394b94cb5ce85e602daf4050
stephentian <745752067@qq.com>
2023-04-25 10:00:22 +0800

    :art: add cloneJSON

--- js/code/deepClone.js
+++ js/code/deepClone.js
@@ -1,18 +1,19 @@
+/**
+ * author: stephentian
+ * email: tianre96@gmail.com
+ * day: 2023-04-25
+ **/
+
 // 深拷贝
 
-function clone(target) {
-  if (typeof target === 'object') {
-    let cloneTarget = Array.isArray(target) ? [] : {}
-    for (const key in target) {
-      cloneTarget[key] = clone(target[key])
-    }
-    return cloneTarget
-  } else {
-    return target
-  }
+// 一行代码的深拷贝
+function cloneJSON(source) {
+	return JSON.parse(JSON.stringify(source))
 }
-
-// 测试
+// 递归爆栈
+// cloneJSON(createData(10000)); // Maximum call stack size exceeded
+// 循环引用 报错
+// cloneJSON(a) // Uncaught TypeError: Converting circular structure to JSON
 
 // 生成指定深度和每层广度的代码
 function createData(deep, breadth = 0) {
@@ -30,14 +31,19 @@ function createData(deep, breadth = 0) {
 }
 // createData(1, 2); // 1层深度，每层有2个数据 {data: {0: 0, 1: 1}}
 
-// 一行代码的深拷贝
-function cloneJSON(source) {
-	return JSON.parse(JSON.stringify(source))
+function clone(target) {
+  if (typeof target === 'object') {
+    let cloneTarget = Array.isArray(target) ? [] : {}
+    for (const key in target) {
+      cloneTarget[key] = clone(target[key])
+    }
+    return cloneTarget
+  } else {
+    return target
+  }
 }
-// 递归爆栈
-// cloneJSON(createData(10000)); // Maximum call stack size exceeded
-// 循环引用 报错
-// cloneJSON(a) // Uncaught TypeError: Converting circular structure to JSON
+
+
 
 // 深拷贝优化
 // 正则、时间类型处理
@@ -48,4 +54,18 @@ function deepClone(obj, hash= new WeakMap()) {
   if (obj === null) return obj
   if (obj instanceof RegExp) return new RegExp(obj)
   if (obj instanceof Date) return new Date(obj)
+  if (typeof obj !== 'object') return obj
+  if (hash.has(obj)) return hash.get(obj) // 处理循环引用
+
+  const copy = new obj.constructor()
+  hash.set(obj, copy)
+
+  for (const key in obj) {
+    // 原型链的值不拷贝
+    if (obj.hasOwnProperty(key)) {
+      copy[key] = deepClone(obj[key], hash)
+    }
+  }
+
+  return copy
 }
