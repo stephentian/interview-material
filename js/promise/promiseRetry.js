746a9a1a855b9d91cd35eb310f3d9543fc801a37
stephentian <745752067@qq.com>
2023-05-09 16:20:13 +0800

    :sparkles: promise retry

---
+++ js/promise/promiseRetry.js
@@ -0,0 +1,27 @@
+/**
+ * author: stephentian
+ * email: tianre96@gmail.com
+ * day: 2023-05-09
+ **/
+
+// 解决同步调用失败重新尝试问题
+// 若在判断次数以内则依据返回结果，否则重新尝试执行随机数生成函数，超过判断次数则抛出异常
+
+Promise.retry = function(fn, times, delay) {
+  return new Promise((resolve, reject) => {
+    const innerFn = function() {
+      fn().then(resolve).catch(e => {
+
+        console.log(`还剩${times}次...`)
+        if (times === 0) {
+          reject(e)
+        } else {
+          times --
+          setTimeout(innerFn, delay)
+        }
+      })
+    }
+
+    return innerFn
+  })
+}
