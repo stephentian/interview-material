f2fdb6bdb3fb632d3f494f65791db3e5155662a1
stephentian <745752067@qq.com>
2023-07-13 16:50:05 +0800

    :pencil: data struct

--- dataStruct/queue.js
+++ dataStruct/queue.js
@@ -1,48 +1,51 @@
 /**
-* @description: 队列
-* @author: stephentian
-* @date: 2022-05-03
-**/
+ * @description: 队列
+ * @author: stephentian
+ * @date: 2022-05-03
+ **/
 
-class myQueue {
-    constructor(items) {
-        this._list = items ? Array.from(items) : [];
-		this._listReversed = [];
-    }
-
-    get length() {
-		return this._list.length + this._listReversed.length;
+class MyQueue {
+	constructor(items) {
+		this._list = items ? Array.from(items) : []
+		this._listReversed = []
 	}
 
-    clear() {
-		this._list.length = 0;
-		this._listReversed.length = 0;
+	get length() {
+		return this._list.length + this._listReversed.length
 	}
 
-    enqueue(item) {
-		this._list.push(item);
+	clear() {
+		this._list.length = 0
+		this._listReversed.length = 0
 	}
-
-    dequeue() {
+	// 入队 向队列尾部添加新的项
+	enqueue(item) {
+		this._list.push(item)
+	}
+	// 出队 移除队列的第一项（即排在队列最前面的项）并返回被移除的元素
+	dequeue() {
 		if (this._listReversed.length === 0) {
-			if (this._list.length === 0) return undefined;
-			if (this._list.length === 1) return this._list.pop();
-			if (this._list.length < 16) return this._list.shift();
-			const temp = this._listReversed;
-			this._listReversed = this._list;
-			this._listReversed.reverse();
-			this._list = temp;
+			if (this._list.length === 0) return undefined
+			if (this._list.length === 1) return this._list.pop()
+			if (this._list.length < 16) return this._list.shift()
+			// webpack 源码优化
+			// 长度大于 16 使用 reverse + pop 代替 shift
+			const temp = this._listReversed
+			this._listReversed = this._list
+			this._listReversed.reverse()
+			this._list = temp
 		}
-		return this._listReversed.pop();
+
+		return this._listReversed.pop()
 	}
 
-    delete(item) {
-		const i = this._list.indexOf(item);
+	delete(item) {
+		const i = this._list.indexOf(item)
 		if (i >= 0) {
-			this._list.splice(i, 1);
+			this._list.splice(i, 1)
 		} else {
-			const i = this._listReversed.indexOf(item);
-			if (i >= 0) this._listReversed.splice(i, 1);
+			const i = this._listReversed.indexOf(item)
+			if (i >= 0) this._listReversed.splice(i, 1)
 		}
 	}
 }
