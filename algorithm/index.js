7979313e2983fb3443e752615ac465f9f477febd
stephentian <stephentian@foxmail.com>
2024-05-30 23:50:44 +0800

    feat: rm debugger

--- algorithm/index.js
+++ algorithm/index.js
@@ -13,7 +13,6 @@
 var addBinary = function (a, b) {
   let ans = ''
   let num = 0 // 进位
-  debugger
   let i = a.length - 1
   let j = b.length - 1
   for (; i >= 0 || j >= 0; i--, j--) {
@@ -26,7 +25,6 @@ var addBinary = function (a, b) {
     sum = sum + m
     sum = sum + n
 
-    debugger
     ans = ans + sum % 2 // 拼接 1
     num = Math.floor(sum / 2)
   }
