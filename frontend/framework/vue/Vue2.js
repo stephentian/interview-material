69ec7b5ab972a10b1a9d4356bfb163befc2228a7
stephentian <stephentian@foxmail.com>
2025-07-11 17:07:18 +0800

    feat: formate js

--- frontend/framework/vue/Vue2.js
+++ frontend/framework/vue/Vue2.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * @description: vue MVVM 框架的简化实现
  * @author: stephen
  * @date: 2022-07-02
@@ -8,24 +8,24 @@
 // es6
 class Vue {
   constructor(options) {
-		this.$options = options;
-		this._data = options.data
-		this._methods = options.methods
+    this.$options = options
+    this._data = options.data
+    this._methods = options.methods
 
     //数据监听
-		observe(this._data)
+    observe(this._data)
 
     // 初始化编译器，负责解析模板并生成渲染函数
-		new Compile(options.el, this)
-	}
+    new Compile(options.el, this)
+  }
 
   /**
    * 创建一个 Observer 实例，用于观测数据对象
    * @param {any} value - 需要被观测的数据对象
    * @returns {Observer} 返回一个 Observer 实例
    */
-	observe(value) {
-    return new Observer(value);
+  observe(value) {
+    return new Observer(value)
   }
 
   /**
@@ -33,9 +33,9 @@ class Vue {
    * @param {Object} vm - Vue 实例
    * @param {string} key - 数据对象的属性名
    */
-	_proxy(vm, key) {
-    Object.keys(this._data).foreach(key => {
-			Object.defineProperty(this._data, key, {
+  _proxy(vm, key) {
+    Object.keys(this._data).foreach((key) => {
+      Object.defineProperty(this._data, key, {
         enumerable: false,
         configurable: true,
         get() {
@@ -43,10 +43,10 @@ class Vue {
         },
         set(newValue) {
           this._data[key] = newValue
-        }
+        },
       })
-		})
-	}
+    })
+  }
 }
 
 class Observer {
@@ -61,7 +61,7 @@ class Observer {
    */
   walk(data) {
     const self = this
-    Object.keys(data).forEach(function(key) {
+    Object.keys(data).forEach(function (key) {
       self.defineReactive(data, key, data[key])
     })
   }
@@ -86,9 +86,8 @@ class Observer {
         return val
       },
       set(newValue) {
-
         dep.notify()
-      }
+      },
     })
   }
 }
@@ -113,7 +112,6 @@ class Dep {
     })
   }
 }
-
 
 /**
  * Watcher 类，用于订阅数据变化并更新视图
@@ -140,65 +138,65 @@ Dep.target = null
 
 /**
  * Compile 类，用于编译模板并且生成渲染函数
-**/
+ **/
 class Compile {
   constructor(el, vm) {
-    this.$vm = vm;
-    this.$el = document.querySelector(el);
+    this.$vm = vm
+    this.$el = document.querySelector(el)
 
     if (this.$el) {
-      this.compileElement(this.$el);
+      this.compileElement(this.$el)
     }
   }
 
   compileElement(el) {
-    const childNodes = el.childNodes;
+    const childNodes = el.childNodes
 
-    childNodes.forEach(child => {
+    childNodes.forEach((child) => {
       if (this.isTextNode(child)) {
         // 编译文本节点 {{ }}
-        this.compileText(child);
+        this.compileText(child)
       } else if (this.isElementNode(child)) {
         // 处理节点指令 v-bind, v-on
-        const attrs = child.attributes;
-        Array.from(attrs).forEach(attr => {
-          const attrName = attr.name;
+        const attrs = child.attributes
+        Array.from(attrs).forEach((attr) => {
+          const attrName = attr.name
           if (this.isDirective(attrName)) {
-            const dir = attrName.substring(2);
-            this[dir] && this[dir](child, attr.value);
+            const dir = attrName.substring(2)
+            this[dir] && this[dir](child, attr.value)
           }
         })
       }
 
       // 递归编译子节点
-      this.compileElement(child);
+      this.compileElement(child)
     })
   }
 
   /**
    * 判断是否味文本节点
-  **/
+   **/
   isTextNode(node) {
-    return node.nodeType === 3;
+    return node.nodeType === 3
   }
 
   isElementNode(node) {
-    return node.nodeType === 1;
+    return node.nodeType === 1
   }
 
   isDirective(attr) {
-    return attr.startsWith('v-');
+    return attr.startsWith('v-')
   }
 
   compileText(node) {
-    const content = node.textContent;
-    const reg = /\{\{(.*)\}\}/;
+    const content = node.textContent
+    const reg = /\{\{(.*)\}\}/
 
     if (reg.test(content)) {
-      const expr = RegExp.$1.trim();
+      const expr = RegExp.$1.trim()
 
       // 更新内容
-      node.textContent = this.$vm[expr];
+      node.textContent = this.$vm[expr]
     }
   }
 
